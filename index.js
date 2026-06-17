20816952f9a5fe208328ec30415daa0816f792db
Quincy-Bee <quincybee@icloud.com>
2026-06-17 14:51:41 -0400

    commiting about

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-import express from "express"
-import path from "path"
-import { fileURLToPath } from "url"
+import express from "express";
+
+import path from "path";
+
+import { fileURLToPath } from "url";
+
+
+
 const app = express();
+
 const port = 3000;
 
 
 
-// Resolve __dirname in an ES module so the views path is absolute
+// Fix __dirname for ES modules
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-// Set up the Pug view engine
+
+
+
+// Set Pug as the view engine
+
 app.set("view engine", "pug");
+
 app.set("views", path.join(__dirname, "views"));
 
 
 
-//view engine
-// app.set("veiw engine", "pug");
+// Home route
 
-// app.set("views", path.join(__dirname, "views"));
+app.get("/", (req, res) => {
 
-app.get("/", (req, res,) => {
-    res.json().render("index", {
+    res.render("index", {
+
         title: "Home"
+
     });
+
 });
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+
+// Start server
+
 app.listen(port, () => {
-    console.log('Listening on port: ' + port)
-})[4:00 PM]This is the index.js(edited)
-[4:01 PM]{
-    "name": "alab-318.2.1",
-        "version": "1.0.0",
-            "description": "",
-                "main": "index.js",
-                    "scripts": {
-        "test": "echo \"Error: no test specified\" && exit 1"
-    },
-    "keywords": [],
-        "author": "",
-            "license": "ISC",
-                "type": "module",
-                    "dependencies": {
-        "ejs": "^6.0.1",
-            "express": "^5.2.1",
-                "pug": "^3.0.4"
-    }
-}
+
+    console.log(`Listening on port: ${port}`);
+
+});
